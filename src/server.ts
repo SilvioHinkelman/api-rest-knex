496a9077a233e27d4dcb92816c36e779a166a6cc
Silvio Hinkelman <silviohink@gmail.com>
2025-10-16 17:20:00 -0300

    first-request

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 import { fastify } from 'fastify'
 import { knex } from './database'
+import crypto from 'node:crypto'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação teste',
+      amount: 1000,
+    })
+    .returning('*')
 
-  return tables
+  return transaction
 })
 
 app
