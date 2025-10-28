2bba9f713f551c4899d985442ecdd409c642fe55
Silvio Hinkelman <silviohink@gmail.com>
2025-10-28 14:59:29 -0300

    create migration and remove after tests

--- src/database.ts
+++ src/database.ts
@@ -4,7 +4,7 @@ import { env } from './env'
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: env.DATABE_URL,
+    filename: env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
--- src/env/index.ts
+++ src/env/index.ts
@@ -1,11 +1,17 @@
-import 'dotenv/config'
+import { config } from 'dotenv'
 import { z } from 'zod'
+
+if (process.env.NODE_ENV !== 'test') {
+  config({ path: '.env.test' })
+} else {
+  config()
+}
 
 const envSchema = z.object({
   NODE_ENV: z
     .enum(['development', 'production', 'test'])
     .default('development'),
-  DATABE_URL: z.string(),
+  DATABASE_URL: z.string(),
   PORT: z.number().default(3333),
 })
 
